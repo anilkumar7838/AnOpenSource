a9c20ddec24fbf0a8e3fd84327e16f713a26cf48
anilkumar7838 <anilkumar9868573282@gmail.com>
2023-12-08 21:44:08 +0530

    Intial Phase

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Footer from "./components/views/footer/footer.js";
+import Home from "./components/Home/Home.js";
+import Contact from "./components/views/Contact/Contact"
+import About from "./components/views/About/About"
+import Navbar from "./components/views/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="app">
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<LoginSignUp />} />
+      </Routes>
+    </Router>
+      <Footer />
+      </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui voluptas aperiam error, maxime officia dolores suscipit praesentium rem rerum, sit aliquam perspiciatis amet maiores officiis. Unde totam voluptatum praesentium necessitatibus perspiciatis rerum distinctio tenetur.</p>
+      <button className='homepage-btn'>Open</button>
+    </div>
+  );
+}
+
+export default Home;
